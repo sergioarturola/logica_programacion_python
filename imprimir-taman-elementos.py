7a6e4cc704e4bcc9fb16257ee363781cd6a3f994
Sergio Arturo <119349223+sergioarturola@users.noreply.github.com>
2023-09-14 12:32:23 -0600

    uso de compresion de listas

---
+++ imprimir-taman-elementos.py
@@ -0,0 +1,6 @@
+#Encontrar el tamaño de los elementos de la lista
+#con esos tamaños crear una lista e imprimirla
+#usar la menor cantidad de lineas
+langs = ['c', 'cpp', 'python', 'java']
+list_len = [len(x) for x in langs ]
+print(list_len)
